68fdb7a91798fce40b094db9c136b770e22b48d2
李思岭 <lisiling@dhcp169.merunorth104.iit.edu>
2015-10-29 19:34:00 -0500

    Added launch-rds.sh

---
+++ launch-rds.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
+
+if [ ${#dbInstanceARR[@]} -gt 0 ]
+   then
+   echo "Deleting existing RDS database-instances"
+   LENGTH=${#dbInstanceARR[@]}
+
+      for (( i=0; i<${LENGTH}; i++));
+      do
+      if [ ${dbInstanceARR[i]} == "lsl-db" ] 
+     then 
+      echo "db exists"
+     else
+     aws rds create-db-instance --db-instance-identifier lsl-db --db-instance-class db.t1.micro --engine MySQL --master-username controller --master-user-password ilovebunnies --allocated-storage 5
+      fi  
+     done
+fi
+
+
