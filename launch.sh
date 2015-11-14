d8deeaead8bd81d4094fb98d09547b603f7e7fd4
李思岭 <lisiling@lisilings-MacBook-Pro.local>
2015-11-14 16:21:48 -0600

    updated db-

--- launch.sh
+++ launch.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-
+#!/usr/local/bin/bash
+#./launch.sh ami-d05e75b8 3 t2.micro sg-bf9a15d9 subnet-1a25d66c itmo544-fall2015 itmo544-role1
 ./cleanup.sh
 
 #declare an array in bash 
 declare -a instanceARR
 
 
-mapfile -t instanceARR < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --key-name $6 --security-group-ids $4 --subnet-id $5 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file://../itmo544-env/install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+mapfile -t instanceARR < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3 --security-group-ids $4 --subnet-id $5 --key-name $6 --associate-public-ip-address --iam-instance-profile Name=$7 --user-data file://../itmo544-env/install-webserver.sh --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
 echo ${instanceARR[@]}
 
 aws ec2 wait instance-running --instance-ids ${instanceARR[@]}
@@ -46,23 +46,19 @@ aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo-544-ext
 
 
 echo -e "\n-create db"
-#aws rds create-db-subnet-group --db-subnet-group-name itmo544 --db-subnet-group-description "itmo544" --subnet-ids $5 subnet-84f892f3
+aws rds create-db-subnet-group --db-subnet-group-name itmo544 --db-subnet-group-description "itmo544" --subnet-ids $5 subnet-f32912d8
 
 mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
 
-#if [ ${#dbInstanceARR[@]} -gt 0 ]
-#   then 
-#echo ${#dbInstanceARR[@]}
    
    LENGTH=${#dbInstanceARR[@]}
-#echo $LENGTH	
        for (( i=0; i<=${LENGTH}; i++));
       do
       if [[ ${dbInstanceARR[i]} == "lsl-db" ]]
      then 
       echo "db exists"
      else
-     aws rds create-db-instance --db-name itmo544mp1 --db-instance-identifier lsl-db --db-instance-class db.t1.micro --engine MySQL --master-username lisiling --master-user-password ilovebunnies --allocated-storage 5
+     aws rds create-db-instance --db-name itmo544mp1 --db-instance-identifier lsl-db --db-instance-class db.t1.micro --engine MySQL --master-username lisiling --master-user-password ilovebunnies --allocated-storage 5 --db-subnet-group-name itmo544 --publicly-accessible
       fi  
       aws rds wait db-instance-available --db-instance-identifier lsl-db
      done  
